60941ac4efa9cf8fc19f655bb0d2bc5309207eb6
Yuze Fu <i@xfox.me>
2024-04-01 11:51:50 +0900

    feat: use encode as a fallback

--- Sources/URLLib/URLLib.swift
+++ Sources/URLLib/URLLib.swift
@@ -1,5 +1,23 @@
 import Foundation
 
 func parseUrl(urlStr: String) -> URL? {
-    return URLComponents(string: urlStr)?.url
+    var urlComponents = URLComponents(string: urlStr)
+    if urlComponents == nil {
+        guard let pUrlStr = urlStr.addingPercentEncoding(withAllowedCharacters: .urlPathAllowed) else { return nil }
+        urlComponents = URLComponents(string: pUrlStr)
+    }
+    return urlComponents?.url
+}
+
+extension CharacterSet {
+  static let urlAllowed: CharacterSet = {
+    var set = CharacterSet.urlHostAllowed
+      .union(.urlUserAllowed)
+      .union(.urlPasswordAllowed)
+      .union(.urlPathAllowed)
+      .union(.urlQueryAllowed)
+      .union(.urlFragmentAllowed)
+    set.insert(charactersIn: "%")
+    return set
+  }()
 }
--- Tests/URLLibTests/TestCase.swift
+++ Tests/URLLibTests/TestCase.swift
@@ -3,10 +3,16 @@ import Foundation
 @testable import URLLib
 
 final class TestCase: XCTestCase {
-    func testHavingContinentLocaleShouldNotReturnInferredLocation() {
+    func testCanHandleUrlWithPreservedCharacters() {
         XCTAssertEqual(
             parseUrl(urlStr: "https://example.com/[foo bar.mkv")?.absoluteString,
             "https://example.com/%5Bfoo%20bar.mkv"
+        )
+    }
+    func testCanHandleUrlWithPercentages() {
+        XCTAssertEqual(
+            parseUrl(urlStr: "https://example.com/%foo bar.mkv")?.absoluteString,
+            "https://example.com/%25foo%20bar.mkv"
         )
     }
 }
