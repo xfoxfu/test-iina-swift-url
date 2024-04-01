1002df0badc2795ce4aae5946f6bb7adf7a6d621
Yuze Fu <i@xfox.me>
2024-04-01 11:54:16 +0900

    fix: use full set of url allowed

--- Sources/URLLib/URLLib.swift
+++ Sources/URLLib/URLLib.swift
@@ -3,7 +3,7 @@ import Foundation
 func parseUrl(urlStr: String) -> URL? {
     var urlComponents = URLComponents(string: urlStr)
     if urlComponents == nil {
-        guard let pUrlStr = urlStr.addingPercentEncoding(withAllowedCharacters: .urlPathAllowed) else { return nil }
+        guard let pUrlStr = urlStr.addingPercentEncoding(withAllowedCharacters: .urlAllowed) else { return nil }
         urlComponents = URLComponents(string: pUrlStr)
     }
     return urlComponents?.url
